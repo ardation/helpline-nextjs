1358d980ba07046cc5ad3b4e3917d5ac614f8aab
Tataihono Nikora <tataihono.nikora@gmail.com>
2020-04-07 22:04:58 +1200

    up the diff threshold

--- src/components/CountrySelect/CountrySelect.stories.tsx
+++ src/components/CountrySelect/CountrySelect.stories.tsx
@@ -4,24 +4,28 @@ import { action } from '@storybook/addon-actions';
 import { Box } from '@material-ui/core';
 import CountrySelect from '.';
 
-storiesOf('CountrySelect', module).add('default', () => {
-    return (
-        <Box m={2}>
-            <CountrySelect
-                countries={[
-                    { code: 'AU', name: 'Australia', subdivisions: [] },
-                    {
-                        code: 'NZ',
-                        name: 'New Zealand',
-                        subdivisions: [
-                            { name: 'Bay of Plenty', code: 'BOP' },
-                            { name: 'Auckland', code: 'AUK' },
-                        ],
-                    },
-                ]}
-                onCountryChange={action('onCountryChange')}
-                onSubdivisionChange={action('onSubdivisionChange')}
-            />
-        </Box>
-    );
-});
+storiesOf('CountrySelect', module).add(
+    'default',
+    () => {
+        return (
+            <Box m={2}>
+                <CountrySelect
+                    countries={[
+                        { code: 'AU', name: 'Australia', subdivisions: [] },
+                        {
+                            code: 'NZ',
+                            name: 'New Zealand',
+                            subdivisions: [
+                                { name: 'Bay of Plenty', code: 'BOP' },
+                                { name: 'Auckland', code: 'AUK' },
+                            ],
+                        },
+                    ]}
+                    onCountryChange={action('onCountryChange')}
+                    onSubdivisionChange={action('onSubdivisionChange')}
+                />
+            </Box>
+        );
+    },
+    { chromatic: { diffThreshold: 0.7 } },
+);
--- src/components/Search/Search.stories.tsx
+++ src/components/Search/Search.stories.tsx
@@ -4,25 +4,29 @@ import { ThemeProvider, Box } from '@material-ui/core';
 import theme from '../../theme';
 import Search from '.';
 
-storiesOf('Search', module).add('default', () => {
-    return (
-        <ThemeProvider theme={theme}>
-            <Box m={2}>
-                <Search
-                    countries={[
-                        { code: 'AU', name: 'Australia', subdivisions: [] },
-                        {
-                            code: 'NZ',
-                            name: 'New Zealand',
-                            subdivisions: [
-                                { name: 'Bay of Plenty', code: 'BOP' },
-                                { name: 'Auckland', code: 'AUK' },
-                            ],
-                        },
-                    ]}
-                    topics={[{ name: 'Anxiety' }, { name: 'Bullying' }]}
-                />
-            </Box>
-        </ThemeProvider>
-    );
-});
+storiesOf('Search', module).add(
+    'default',
+    () => {
+        return (
+            <ThemeProvider theme={theme}>
+                <Box m={2}>
+                    <Search
+                        countries={[
+                            { code: 'AU', name: 'Australia', subdivisions: [] },
+                            {
+                                code: 'NZ',
+                                name: 'New Zealand',
+                                subdivisions: [
+                                    { name: 'Bay of Plenty', code: 'BOP' },
+                                    { name: 'Auckland', code: 'AUK' },
+                                ],
+                            },
+                        ]}
+                        topics={[{ name: 'Anxiety' }, { name: 'Bullying' }]}
+                    />
+                </Box>
+            </ThemeProvider>
+        );
+    },
+    { chromatic: { diffThreshold: 0.7 } },
+);
