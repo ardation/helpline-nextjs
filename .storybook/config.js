485fc5e624c9eab47fd6aab9f750e9cdc37788c1
Tataihono Nikora <tataihono.nikora@gmail.com>
2020-04-07 21:46:51 +1200

    add viewports

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,8 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
+
+addParameters({
+  chromatic: { viewports: [320, 414, 834, 1200] },
+});
+
 // automatically import all files ending in *.stories.tsx
 configure(require.context('../src/components', true, /\.stories\.tsx?$/), module);
