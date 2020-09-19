7da0cbfeb291b4c0c1cd0b2da3c2ce09c15b52b6
Tataihono Nikora <tataihono.nikora@gmail.com>
2020-09-19 13:28:20 +1200

    change home to search

--- src/components/Placeholder/Placeholder.stories.tsx
+++
@@ -1,8 +0,0 @@
-import React, { ReactElement } from 'react';
-import Placeholder from '.';
-
-export default {
-    title: 'Placeholder',
-};
-
-export const Default = (): ReactElement => <Placeholder />;
