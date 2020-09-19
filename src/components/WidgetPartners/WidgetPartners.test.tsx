3d236b13210e98450738802c663b72fe472ea913
Tataihono Nikora <tataihono.nikora@gmail.com>
2020-09-19 21:19:48 +1200

    update name

--- src/components/WidgetPartners/WidgetPartners.test.tsx
+++ src/components/WidgetPartners/WidgetPartners.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import WidgetPartners from '.';
 
-describe('About', () => {
+describe('WidgetPartners', () => {
     it('should contain Get in touch link', () => {
         const { getByText } = render(<WidgetPartners />);
         expect(getByText('Get in touch').parentElement).toHaveAttribute('href', '/contact');
