14f37573e30c521951cdd93bf53e2ef6478d5c92
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-05-24 22:08:04 +1200

    add donate button

---
+++ src/components/Donate/Donate.tsx
@@ -0,0 +1,52 @@
+import { Button, createStyles, makeStyles, SvgIcon } from '@material-ui/core';
+import { loadStripe } from '@stripe/stripe-js';
+import React, { ReactElement } from 'react';
+import CallIcon from '../../assets/volunteer-activism-icon.svg';
+
+const useStyles = makeStyles(() =>
+    createStyles({
+        button: {
+            textDecoration: 'underline',
+        },
+        svgIcon: {
+            width: '0.9em',
+            height: '0.9em',
+        },
+    }),
+);
+
+const Donate = (): ReactElement => {
+    const classes = useStyles();
+
+    const handleClick = async (): Promise<void> => {
+        const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+
+        stripe.redirectToCheckout({
+            lineItems: [
+                {
+                    price: process.env.NEXT_PUBLIC_STRIPE_PRICE_ID,
+                    quantity: 1,
+                },
+            ],
+            mode: 'payment',
+            successUrl: `${window.location.protocol}//${window.location.host}/success`,
+            cancelUrl: `${window.location.protocol}//${window.location.host}/cancel`,
+        });
+    };
+    return (
+        <Button
+            onClick={handleClick}
+            fullWidth
+            startIcon={
+                <SvgIcon className={classes.svgIcon}>
+                    <CallIcon />
+                </SvgIcon>
+            }
+            className={classes.button}
+        >
+            Donate $1 to save lives
+        </Button>
+    );
+};
+
+export default Donate;
