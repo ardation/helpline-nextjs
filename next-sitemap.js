b303f673e26fb72a9203105a91ed96e6c010badc
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-07-12 08:59:14 +1200

    add proper excludes

--- next-sitemap.js
+++ next-sitemap.js
@@ -2,7 +2,7 @@ const siteUrl = process.env.SITE_URL || 'https://findahelpline.com';
 module.exports = {
     siteUrl,
     generateRobotsTxt: true,
-    exclude: ['/server-sitemap.xml'],
+    exclude: ['/widget', '/widget/*', '/i/*', '/server-sitemap.xml'],
     sitemapSize: 7000,
     robotsTxtOptions: {
         additionalSitemaps: [`${siteUrl}/server-sitemap.xml`],
