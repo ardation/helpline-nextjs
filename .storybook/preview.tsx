4363771d3e7fd94b82dbbd82db1f4e1aa00ace5e
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-08-21 11:04:04 +1200

    fix story images

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -4,6 +4,13 @@ import { addDecorator } from '@storybook/react';
 import { ThemeProvider } from '@material-ui/core';
 import theme from '../src/theme';
 import stubOrganizationReviewCreate from '../tests/stubs/stubOrganizationReviewCreate';
+import * as NextImage from 'next/image';
+
+const OriginalNextImage = NextImage.default;
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
 
 stubOrganizationReviewCreate();
 addDecorator(storyFn => <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>);
